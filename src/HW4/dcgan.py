322b34646c9e1ddf00c55ebb9871fc33cf3924d8
nikita488 <6237881+nikita488@users.noreply.github.com>
2025-06-23 17:10:38 +0300

    Modified DCGAN and added results

--- src/HW4/dcgan.py
+++ src/HW4/dcgan.py
@@ -1,36 +1,47 @@
 import argparse
 import os
-import numpy as np
-import math
+import random
 
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 
-from torch.utils.data import DataLoader
 from torchvision import datasets
-from torch.autograd import Variable
 
 import torch.nn as nn
-import torch.nn.functional as F
 import torch
 
 from pathlib import Path
-from utils import save_checkpoint, load_checkpoint
+from utils import save_checkpoint, CheckpointLoader
+
+manual_seed = 42
+random.seed(manual_seed)
+torch.manual_seed(manual_seed)
+torch.use_deterministic_algorithms(True)
+
+save_path = Path.cwd()
+checkpoints_path = save_path / "checkpoints"
+images_path = save_path / "images"
+
+checkpoints_path.mkdir(parents=True, exist_ok=True)
+images_path.mkdir(parents=True, exist_ok=True)
+
+checkpoint_loader: CheckpointLoader = None#CheckpointLoader(checkpoints_path / "checkpoint_50.tar")
 
 cli = False
 
 if cli:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-    parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
-    parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+    parser.add_argument("--n_epochs", type=int, default=250, help="number of epochs of training")
+    parser.add_argument("--batch_size", type=int, default=256, help="size of the batches")
+    parser.add_argument("--lr", type=float, default=0.0005, help="adam: learning rate")
     parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
     parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-    parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
-    parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
+    parser.add_argument("--noise_channels", type=int, default=256, help="")
+    parser.add_argument("--g_features", type=int, default=64, help="generator features")
+    parser.add_argument("--d_features", type=int, default=64, help="discriminator features")
+    parser.add_argument("--img_size", type=int, default=64, help="size of each image dimension")
     parser.add_argument("--channels", type=int, default=1, help="number of image channels")
-    parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+    parser.add_argument("--sample_interval", type=int, default=50, help="interval between image sampling")
     parser.add_argument("--checkpoint_interval", type=int, default=10, help="interval between checkpoint saving")
     opt = parser.parse_args()
     print(opt)
@@ -40,16 +51,17 @@ else:
     opt.batch_size = 64
     opt.lr = 0.0002
     opt.b1 = 0.5
-    opt.b2 = 0.99
-    opt.n_cpu = 8
-    opt.latent_dim = 100
-    opt.img_size = 32
+    opt.b2 = 0.999
+    opt.noise_channels = 100
+    opt.g_features = 64
+    opt.d_features = 64
+    opt.img_size = 28
     opt.channels = 1
-    opt.sample_interval = 400
-    opt.checkpoint_interval = 10
+    opt.sample_interval = 50
+    opt.checkpoint_interval = 5
     print(opt)
 
-cuda = True if torch.cuda.is_available() else False
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def weights_init_normal(m):
@@ -65,57 +77,46 @@ class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
-        self.init_size = opt.img_size // 4
-        self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128 * self.init_size ** 2))
-
-        self.conv_blocks = nn.Sequential(
-            nn.BatchNorm2d(128),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 128, 3, stride=1, padding=1),
-            nn.BatchNorm2d(128, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 64, 3, stride=1, padding=1),
-            nn.BatchNorm2d(64, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, opt.channels, 3, stride=1, padding=1),
+        self.model = nn.Sequential(
+            nn.ConvTranspose2d(opt.noise_channels, opt.g_features * 8, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(opt.g_features * 8),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(opt.g_features * 8, opt.g_features * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(opt.g_features * 4),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(opt.g_features * 4, opt.g_features * 2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(opt.g_features * 2),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(opt.g_features * 2, opt.g_features, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(opt.g_features),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(opt.g_features, opt.channels, kernel_size=1, stride=1, padding=2, bias=False),
             nn.Tanh(),
         )
 
-    def forward(self, z):
-        out = self.l1(z)
-        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
-        img = self.conv_blocks(out)
-        return img
+    def forward(self, x):
+        return self.model(x)
 
 
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
 
-        def discriminator_block(in_filters, out_filters, bn=True):
-            block = [nn.Conv2d(in_filters, out_filters, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
-            if bn:
-                block.append(nn.BatchNorm2d(out_filters, 0.8))
-            return block
-
         self.model = nn.Sequential(
-            *discriminator_block(opt.channels, 16, bn=False),
-            *discriminator_block(16, 32),
-            *discriminator_block(32, 64),
-            *discriminator_block(64, 128),
+            nn.Conv2d(opt.channels, opt.d_features, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(opt.d_features, opt.d_features * 2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(opt.d_features * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(opt.d_features * 2, opt.d_features * 4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(opt.d_features * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(opt.d_features * 4, 1, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Sigmoid(),
         )
 
-        # The height and width of downsampled image
-        ds_size = opt.img_size // 2 ** 4
-        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
-
-    def forward(self, img):
-        out = self.model(img)
-        out = out.view(out.shape[0], -1)
-        validity = self.adv_layer(out)
-
-        return validity
+    def forward(self, x):
+        return self.model(x)
 
 
 # Loss function
@@ -125,14 +126,22 @@ adversarial_loss = torch.nn.BCELoss()
 generator = Generator()
 discriminator = Discriminator()
 
-if cuda:
-    generator.cuda()
-    discriminator.cuda()
-    adversarial_loss.cuda()
-
 # Initialize weights
 generator.apply(weights_init_normal)
 discriminator.apply(weights_init_normal)
+
+if checkpoint_loader:
+    checkpoint_loader.load_models(generator, discriminator)
+
+generator = generator.to(device)
+discriminator = discriminator.to(device)
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+#if checkpoint_loader:
+#    checkpoint_loader.load_optimizers(optimizer_G, optimizer_D)
 
 # Configure data loader
 os.makedirs("./data", exist_ok=True)
@@ -142,93 +151,67 @@ dataloader = torch.utils.data.DataLoader(
         train=True,
         download=True,
         transform=transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
         ),
     ),
     batch_size=opt.batch_size,
     shuffle=True,
 )
 
-# Optimizers
-optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
-
-Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
-
-save_path = Path.cwd()
-checkpoints_path = save_path / "checkpoints"
-images_path = save_path / "images"
-
-checkpoints_path.mkdir(parents=True, exist_ok=True)
-images_path.mkdir(parents=True, exist_ok=True)
+fixed_noise = torch.randn(64, opt.noise_channels, 1, 1, device=device)
+real_label = 1.
+fake_label = 0.
 
 start_epoch = 0
 
-# ----------
-#  Loading
-# ----------
-
-checkpoint_path = None #save_path / "checkpoint.tar"
-
-if checkpoint_path:
-    start_epoch = load_checkpoint(checkpoint_path, generator, optimizer_G, discriminator, optimizer_D)
-    generator.train()
-    discriminator.train()
+if checkpoint_loader:
+    start_epoch = checkpoint_loader.load_epoch()
 
 # ----------
 #  Training
 # ----------
 
 for epoch in range(start_epoch, opt.n_epochs, 1):
+    generator.train()
+    discriminator.train()
+    
     for i, (imgs, _) in enumerate(dataloader):
+        discriminator.zero_grad()
+        real_cpu = imgs.to(device)
+        batch_size = real_cpu.size(0)
+        label = torch.full((batch_size,), real_label, device=device)
 
-        # Adversarial ground truths
-        valid = Variable(Tensor(imgs.shape[0], 1).fill_(1.0), requires_grad=False)
-        fake = Variable(Tensor(imgs.shape[0], 1).fill_(0.0), requires_grad=False)
+        output = discriminator(real_cpu).view(-1)
+        real_d_loss = adversarial_loss(output, label)
+        real_d_loss.backward()
 
-        # Configure input
-        real_imgs = Variable(imgs.type(Tensor))
+        noise = torch.randn(batch_size, opt.noise_channels, 1, 1, device=device)
+        fake = generator(noise)
+        label.fill_(fake_label)
+        output = discriminator(fake.detach()).view(-1)
+        fake_d_loss = adversarial_loss(output, label)
+        fake_d_loss.backward()
+        d_loss = real_d_loss + fake_d_loss
+        optimizer_D.step()
 
-        # -----------------
-        #  Train Generator
-        # -----------------
-
-        optimizer_G.zero_grad()
-
-        # Sample noise as generator input
-        z = Variable(Tensor(np.random.normal(0, 1, (imgs.shape[0], opt.latent_dim))))
-
-        # Generate a batch of images
-        gen_imgs = generator(z)
-
-        # Loss measures generator's ability to fool the discriminator
-        g_loss = adversarial_loss(discriminator(gen_imgs), valid)
-
+        generator.zero_grad()
+        label.fill_(real_label) 
+        output = discriminator(fake).view(-1)
+        g_loss = adversarial_loss(output, label)
         g_loss.backward()
         optimizer_G.step()
 
-        # ---------------------
-        #  Train Discriminator
-        # ---------------------
-
-        optimizer_D.zero_grad()
-
-        # Measure discriminator's ability to classify real from generated samples
-        real_loss = adversarial_loss(discriminator(real_imgs), valid)
-        fake_loss = adversarial_loss(discriminator(gen_imgs.detach()), fake)
-        d_loss = (real_loss + fake_loss) / 2
-
-        d_loss.backward()
-        optimizer_D.step()
-
-        print(
-            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
-            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
-        )
-
         batches_done = epoch * len(dataloader) + i
-        if batches_done % opt.sample_interval == 0:
-            save_image(gen_imgs.data[:25], images_path / f"{epoch}_{batches_done}.png", nrow=5, normalize=True)
+        if i % opt.sample_interval == 0:
+            with torch.no_grad():
+                generator.eval()
+                fake_images = generator(fixed_noise).detach().cpu()
+                save_image(fake_images[:25], images_path / f"{epoch}_{batches_done}.png", nrow=5, normalize=True)
     
+    print(
+        "[Epoch %d/%d] [D loss: %f] [G loss: %f]"
+        % (epoch + 1, opt.n_epochs, d_loss.item(), g_loss.item())
+    )
+
     if epoch % opt.checkpoint_interval == 0:
         save_checkpoint(checkpoints_path / f"checkpoint_{epoch}.tar", epoch, generator, optimizer_G, discriminator, optimizer_D)
--- src/HW4/utils.py
+++ src/HW4/utils.py
@@ -6,14 +6,6 @@ D_MODEL_STATE_KEY = 'd_model_state'
 G_OPTIMIZER_STATE_KEY = 'g_optimizer_state'
 D_OPTIMIZER_STATE_KEY = 'd_optimizer_state'
 
-def load_checkpoint(path, g_model, g_optimizer, d_model, d_optimizer):
-    checkpoint = torch.load(path, weights_only=True)
-    g_model.load_state_dict(checkpoint[G_MODEL_STATE_KEY])
-    d_model.load_state_dict(checkpoint[D_MODEL_STATE_KEY])
-    g_optimizer.load_state_dict(checkpoint[G_OPTIMIZER_STATE_KEY])
-    d_optimizer.load_state_dict(checkpoint[D_OPTIMIZER_STATE_KEY])
-    return checkpoint[EPOCH_KEY]
-
 def save_checkpoint(path, epoch, g_model, g_optimizer, d_model, d_optimizer):
     checkpoint_data = {
         EPOCH_KEY: epoch,
@@ -24,3 +16,18 @@ def save_checkpoint(path, epoch, g_model, g_optimizer, d_model, d_optimizer):
     }
 
     torch.save(checkpoint_data, path)
+
+class CheckpointLoader:
+    def __init__(self, path):
+        self.checkpoint = torch.load(path, weights_only=True)
+    
+    def load_models(self, g_model, d_model):
+        g_model.load_state_dict(self.checkpoint[G_MODEL_STATE_KEY])
+        d_model.load_state_dict(self.checkpoint[D_MODEL_STATE_KEY])
+
+    def load_optimizers(self, g_optimizer, d_optimizer):
+        g_optimizer.load_state_dict(self.checkpoint[G_OPTIMIZER_STATE_KEY])
+        d_optimizer.load_state_dict(self.checkpoint[D_OPTIMIZER_STATE_KEY])
+
+    def load_epoch(self):
+        return self.checkpoint[EPOCH_KEY]
